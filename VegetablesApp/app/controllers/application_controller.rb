6ec0d98add96f50a36d71da664cd3413b544a6cb
Joan Jara <jaraboschj@gmail.com>
2020-06-11 12:35:12 +0200

    Falta fer el diccionari mortal i afegirho als html de ruby

--- VegetablesApp/app/controllers/application_controller.rb
+++ VegetablesApp/app/controllers/application_controller.rb
@@ -15,14 +15,31 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
+    locale = extract_locale_from_accept_language_header
+    variable =  if locale_valid?(locale)
+                   locale
+                 else
+                   :es
+                 end
     if current_client
-      I18n.locale = current_client.preference || I18n.default_locale
+      I18n.locale = current_client.preference || variable
     else
-      I18n.locale = I18n.default_locale
+      I18n.locale = variable
     end
   end
 
   def default_url_options(options={})
     {locale: I18n.locale}.merge options
+  end
+
+  def locale_valid?(locale)
+    I18n.available_locales.map(&:to_s).include?(locale)
+  end
+
+  def extract_locale_from_accept_language_header
+    accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+    return unless accept_language
+
+    accept_language.scan(/^[a-z]{2}/).first
   end
 end
