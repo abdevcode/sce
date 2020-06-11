c9c76da388fced3414c039ed31ab32dffbba525f
Joan Jara <jaraboschj@gmail.com>
2020-06-11 16:00:16 +0200

    Ara falta traduir headers, footers i la pagina de index del producte. Un cop fet aixo, faltara afegir css per els formularis i eliminar css sobrant i ja estara.

--- VegetablesApp/app/controllers/application_controller.rb
+++ VegetablesApp/app/controllers/application_controller.rb
@@ -12,6 +12,27 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def get_cart_information
+    # Obtenemos los productos de la cesta
+    if session[:products]
+      @productscart = Product.find session[:products]
+      # Calculamos el total de productos
+      @numproducts = session[:products].count.to_i
+      @hayproducts = @numproducts > 0
+
+      @totalprice = 0.0
+
+      # Calculamos el precio total de la cesta
+      @productscart.uniq.each do |product|
+        # Multiplicacmos el precio por la cantidad seleccionada
+        @totalprice += product.price.to_f * session[:products].count( product.id ).to_i
+      end
+
+      # Redondeamos a 2 decimales el precio
+      @totalprice = @totalprice.round(2)
+    end
+  end
+
   private
 
   def set_locale
